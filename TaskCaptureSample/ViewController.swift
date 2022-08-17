0daebc50080d9699a0e79167386b187433f862b7
Akira <akira.matsuda@me.com>
2022-08-17 09:39:34 +0900

    Initial Commit

---
+++ TaskCaptureSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskCaptureSample
+//
+//  Created by Akira Matsuda on 2022/08/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
